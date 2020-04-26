a5a1febf384a122319201d6492c399875c326545
itsvinayak <itssvinayak@gmail.com>
2020-04-26 21:22:50 +0530

    Updates

---
+++ precache-manifest.f0a060b656bc06425976c76e8fbd0af7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c734acf7fabbc6140e157458c9e1458a",
+    "url": "/ReactTodo/index.html"
+  },
+  {
+    "revision": "abee42bdf56a53b90d4e",
+    "url": "/ReactTodo/static/js/2.0d624a61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactTodo/static/js/2.0d624a61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30ef72fab23bda965001",
+    "url": "/ReactTodo/static/js/main.09385ee6.chunk.js"
+  },
+  {
+    "revision": "f7b6400d45a5757c5838",
+    "url": "/ReactTodo/static/js/runtime-main.e9c229fb.js"
+  }
+]);
